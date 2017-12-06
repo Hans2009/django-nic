e41e2bb05a715b68cb93c79f29037483b9a7d077
Hans2009 <lishuaitj@126.com>
2017-12-06 06:24:24 -0800

    ssh_cmd

--- bbnic_autotest/bbnic_autotest/if_fb.py
+++ bbnic_autotest/bbnic_autotest/if_fb.py
@@ -2,11 +2,16 @@
 from django.shortcuts import render
 from django.views.decorators import csrf
 import main
+import sys
+sys.path.insert(0,'./tool/')
+sys.path.insert(0,'./testcase/')
+import ssh_cmd
 
 # receive POST request data
 def if_top(request):
     ctx ={}
     if request.POST:
-       # ctx['rlt'] = request.POST['q']
-        ctx['rlt'] = main.welcome_message()
-    return render(request, "if_fb.html", ctx)
+        #ctx['rlt'] = request.POST['q']
+        #ctx['rlt'] = main.welcome_message()
+        ctx['rlt'] = ssh_cmd.ssh_cmd_send('192.168.182.129','22','root','1111111','ifconfig') 
+        return render(request, "if_fb.html", ctx)
