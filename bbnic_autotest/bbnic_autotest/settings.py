3c0d460b9e39e1b26bb4af4987ef975382807e03
Hans2009 <lishuaitj@126.com>
2017-12-05 23:40:55 -0800

    add template

--- bbnic_autotest/bbnic_autotest/settings.py
+++ bbnic_autotest/bbnic_autotest/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'bbnic_autotest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR+"/templates",],   #modify
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- bbnic_autotest/bbnic_autotest/view.py
+++ bbnic_autotest/bbnic_autotest/view.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello world!")
-
+   # return HttpResponse("Hello world!")
+    context = {}
+    context['hello'] = 'Hello World! This is from template'
+    return render(request, 'hello.html',context)
